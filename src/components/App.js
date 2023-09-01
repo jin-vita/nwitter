9bbff276da47694eb6690b761c3d1fc8178299b3
jin-vita <jhj9144@gmail.com>
2023-09-01 13:25:01 +0900

    feat login form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import AppRouter from "./Router";
+import AppRouter from "components/Router";
+import { auth } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState();
+  console.log(auth.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
