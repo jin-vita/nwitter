e7512eda86aa9ae194e653e48698cfa53884b1f2
VITA-R9 <jhj9144@gmail.com>
2023-09-12 07:24:19 +0900

    feat connect with firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,35 @@
+import { async } from "@firebase/util";
+import { firestore } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    if (nweet.trim() === "") {
+      setNweet("");
+      return;
+    }
+    try {
+      await addDoc(collection(firestore, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setNweet("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setNweet(value);
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -26,4 +45,5 @@ const Home = () => {
     </div>
   );
 };
+
 export default Home;
